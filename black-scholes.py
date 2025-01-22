7b922053b100421c33e2a10c6f48b6b17ce49c8a
Jeffrey Lin <114444093+jjl9895@users.noreply.github.com>
2025-01-21 22:45:09 -0500

    Created new file 'black-scholes.py'

---
+++ black-scholes.py
@@ -0,0 +1,24 @@
+import math 
+from scipy.stats import norm
+
+
+def black_scholes(S, K, T, r, sigma):
+    """
+    S: stock price
+    K: strike price
+    T: time to maturity
+    r: risk-free rate
+    sigma: volatility
+    """
+    
+
+    d1 = (math.log(S / K) + (r + 0.5 * (sigma ** 2)/2) * T) / (sigma * math.sqrt(T))
+    d2 = d1 - sigma * math.sqrt(T)
+
+
+    call_price = S * norm.cdf(d1) - K * math.exp(-r * T) * norm.cdf(d2)
+    put_price = K * math.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+
+    return call_price, put_price
+
+
